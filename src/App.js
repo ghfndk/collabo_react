9038096c1572f65f7a973f1f5fd9c7ad7c0de856
byeongjin <ghfndk@example.com>
2025-09-18 17:09:45 +0900

    collabo_react 2025/09/18

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
-
 import './App.css';
 
 // 분리된 리엑트컴포넌트 import
-import MenuItems from './ui/MenuItems' ;
+import MenuItems from './ui/MenuItems';
 import AppRoutes from './routes/AppRoutes'
 
 function App() {
@@ -11,14 +9,7 @@ function App() {
 
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href='/'>{appName}</Navbar.Brand>
-          <Nav className="me_auto">
-            <MenuItems />
-          </Nav>
-        </Container>
-      </Navbar>
+      <MenuItems appName={appName} />
 
       {/* 분리된 라우터 정보 */}
       <AppRoutes />
---
+++ src/pages/Element.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_BASE_URL } from "../config/config";
+import { Table } from "react-bootstrap";
+
+function App() {
+    const [element, setElement] = useState({});
+
+    useEffect(() => {
+        const url = `${API_BASE_URL}/element`;
+
+        axios
+            .get(url, {})
+            .then((response) => {
+                setElement(response.data);
+            });
+    }, [])
+    return (
+        <>
+            <Table hover style={{ margin: '5px' }}>
+                <tbody>
+                    <tr>
+                        <td>아이디</td>
+                        <td>{element.id}</td>
+                    </tr>
+                    <tr>
+                        <td>가격</td>
+                        <td>{Number(element.price).toLocaleString()}원</td>
+                    </tr>
+                    <tr>
+                        <td>카테고리</td>
+                        <td>{element.category}</td>
+                    </tr>
+                    <tr>
+                        <td>재고</td>
+                        <td>{Number(element.stock).toLocaleString()}개</td>
+                    </tr>
+                    <tr>
+                        <td>이미지</td>
+                        <td>{element.image}</td>
+                    </tr>
+                    <tr>
+                        <td>세부 설명</td>
+                        <td>{element.description}</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
+    );
+}
+
+export default App;
