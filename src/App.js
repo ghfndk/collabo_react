0b4195f7f7660fd3d1cbbd75c982261615fd95d4
byeongjin <ghfndk@example.com>
2025-09-23 16:50:47 +0900

    collabo react 2025/09/23

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import MenuItems from './ui/MenuItems';
 import AppRoutes from './routes/AppRoutes'
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   const appName = "It Academy Coffee Shop";
@@ -13,28 +14,36 @@ function App() {
   const [user, setUser] = useState(null);
 
   // JSON.parse()는 JSON 형태의 문자열을 자바 스크립트 객체 형태로 변환해 줍니다.
-  useEffect(()=>{
+  useEffect(() => {
     const loginUser = localStorage.getItem('user');
     setUser(JSON.parse(loginUser));
-  },[]); // 2번째 매개 변수가 empty array이므로 1번만 readering됩니다.
+  }, []); // 2번째 매개 변수가 empty array이므로 1번만 readering됩니다.
 
-  const handleLoginSuccess = (userData)=>{
+  const handleLoginSuccess = (userData) => {
     // userData : LoginPage.js에서 반환 받은 member 정보 입니다.
     setUser(userData);
     localStorage.setItem('user', JSON.stringify(userData));
     console.log('로그인 성공');
   }
 
-  const handleLogoutSuccess = () => {
+  const navigate = useNavigate();
+
+  // 로그인한 사용자가 '로그 아웃' 버튼을 클릭했습니다.
+  const handleLogout = (event) => {
+    event.preventDefault();
+
     setUser(null);
-  }
+    localStorage.removeItem('user');
+    console.log('로그 아웃 성공');
+    navigate(`/member/login`);
+  };
 
   return (
     <>
-      <MenuItems appName={appName} user={user}/>
+      <MenuItems appName={appName} user={user} handleLogout={handleLogout} />
 
       {/* 분리된 라우터 정보 */}
-      <AppRoutes user={user} handleLoginSuccess={handleLoginSuccess} logout={handleLogoutSuccess}/>
+      <AppRoutes user={user} handleLoginSuccess={handleLoginSuccess} />
 
       <footer className="bg-dark text-light text-center py-3 mt-5">
         <p>&copy; 2025 {appName}. All rights reserved.</p>
